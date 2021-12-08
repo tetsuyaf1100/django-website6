041f8bf7795e08e9662d631b1f3912dfe81abc5b
tetsuya <tetsuyaf1100@gmail.com>
2021-12-08 20:18:47 +0900

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
